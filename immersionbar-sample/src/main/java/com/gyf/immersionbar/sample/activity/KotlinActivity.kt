6be81d54482735574672df06fdd952d2aff90f94
vulture <lanjianwei1992@gmail.com>
2020-09-09 13:14:41 +0800

    migrate to androidx, butterknife upgrate to 10.2.1

--- immersionbar-sample/src/main/java/com/gyf/immersionbar/sample/activity/KotlinActivity.kt
+++ immersionbar-sample/src/main/java/com/gyf/immersionbar/sample/activity/KotlinActivity.kt
@@ -2,8 +2,8 @@ package com.gyf.immersionbar.sample.activity
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
-import android.support.v4.content.ContextCompat
-import android.support.v4.view.ViewCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
